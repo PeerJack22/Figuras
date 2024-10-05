dab0a781246d672573b82ec4eb48584eac5a0f3e
PeerJack22 <pajocina.11@gmail.com>
2024-10-05 18:12:59 -0500

    Menú y área del circulo

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,33 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //Empezar código
+        //Menú del código
+        System.out.println("---------------------------------");
+        System.out.println("-Área--de--figuras--geométricas--");
+        System.out.println("---------------------------------");
+        System.out.println("De que figura desea calcular el área: ");
+        System.out.println();
+        System.out.println("1.-Triángulo");
+        System.out.println("2.-Cuadrado");
+        System.out.println("3.-Círculo");
+        System.out.println("4.-Ninguno");
+        //elección de calculo
+        int opcion = sc.nextInt();
+        //Condiciones
+        if (opcion == 1) {
+
+        } else if (opcion == 2) {
+
+        } else if (opcion == 3) {
+            //Área del círculo
+            System.out.println("------Círculo------");
+            System.out.println("Ingrese el radio del círculo: ");
+            double radio = sc.nextDouble();
+            double area = Math.PI * radio * radio;
+            System.out.println("El área del circulo es: " + area);
+        } else if (opcion == 4) {
+
+        }
 
     }
 }
